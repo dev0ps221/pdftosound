3c4a0f4ec407b09122cf00fc393e905447bce4d5
dev0ps221 <seybalofficiel@gmail.com>
2023-05-14 12:13:30 +0000

    check dependencies and stuf::[[these funcs might be useful in future scripts]]

--- p2sound.py
+++ p2sound.py
@@ -14,3 +14,24 @@
 #from reddit conversation:https://www.reddit.com/r/linux4noobs/comments/cmo2i4/whats_a_good_linux_pdf_app_that_can_read_pdfs/
 #
 #
+
+
+deps = {
+        'pdftotext':'',
+    'espeak':'espeak'
+}
+missingdeps = []
+def check_dep(name):
+    """Check whether `name` is on PATH."""
+    from distutils.spawn import find_executable
+    return find_executable(name) is not None
+
+def install_dep(name):
+
+def check_deps(deps):
+    results = {}
+    for dep in deps:
+        results[dep] = check_dep(dep)
+    return results
+
+
