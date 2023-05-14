5abd84dab08318717e8fd9e17fcb41e22ba3f731
dev0ps221 <seybalofficiel@gmail.com>
2023-05-14 12:59:40 +0000

    works

--- p2sound.py
+++ p2sound.py
@@ -1,29 +1,4 @@
 #!/usr/bin/env python3
-#THIS IS FOR LINUX USERS
-#
-#
-#DEPENDENCIES
-#python3
-#poppler-utils
-#espeak
-#
-#reference:
-#" 
-#  pdftotext ebook.pdf ebook.txt
-#  espeak -f ebook.txt -w ebook_audio.wav
-#"
-#from reddit conversation:https://www.reddit.com/r/linux4noobs/comments/cmo2i4/whats_a_good_linux_pdf_app_that_can_read_pdfs/
-#
-#
-#installation
-#git clone https://github.com/dev0ps221/pdftosound
-#cd pdftosound
-#chmod +x p2sound.py
-#
-#usage:
-#   ./p2sound.py sourcefile.pdf destfile.wav
-#
-#
 
 
 from sys import argv
