4bda85c958cde77b51c8c0670e323d3f5f277dd2
dev0ps221 <seybalofficiel@gmail.com>
2023-05-14 11:57:40 +0000

    start

---
+++ p2sound.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#THIS IS FOR LINUX USERS
+#
+#
+#DEPENDENCIES
+#poppler-utils
+#espeak
+#
+#reference:
+#" 
+#  pdftotext ebook.pdf ebook.txt
+#  espeak -f ebook.txt -w ebook_audio.wav
+#"
+#from reddit conversation:https://www.reddit.com/r/linux4noobs/comments/cmo2i4/whats_a_good_linux_pdf_app_that_can_read_pdfs/
+#
+#
